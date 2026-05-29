0b1f4d3bf4d53d4827956b9cd77577e4644d500c
cristiancaso60-coder <cristiancaso60@gmail.com>
2026-05-29 14:27:54 +0200

    Inserimento combo del Player

--- Stage_Project_2D/Assets/Settings/Script/HeroKnight.cs
+++ Stage_Project_2D/Assets/Settings/Script/HeroKnight.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,7 +8,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer spriteRenderer;
+   
     private bool IsMoving;
+    private bool IsAttacking;
+
+    private int comboStep = 0;
+    private float comboTimer = 0f;
+    public float comboResetTime = 0.8f;
 
     private float movementX;
     private float movementY;
@@ -28,58 +35,101 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(movementX * speed, movementY * speed);
         
         
-        Animazioni(rb);
        
-
-        // MOVIMENTO ORIZZONTALE
         if (rb.velocity.x > 0 )
 
         {
             IsMoving = true;
             spriteRenderer.flipX = false;
-            
-            
-
         }
         else if (rb.velocity.x < 0)
         {
             IsMoving = true;
             spriteRenderer.flipX = true;
-            
         }
-        else if(rb.velocity.y > 0)
-        {
-            IsMoving = true;
-
-        }
-
-         else if(rb.velocity.y < 0)
-        {
-            IsMoving = true;
-
-        }
-        
         else
         {
             IsMoving = false;
         }
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleAttackInput();
+        }
+
+        if (comboTimer > 0)
+        {
+            comboTimer -= Time.deltaTime;
+
+            if (comboTimer <= 0)
+            {
+                comboStep = 0; // reset combo
+            }
+        }
+
+        Animazioni();
     }
 
-   private void Animazioni(Rigidbody2D rb)
+   private void Animazioni()
 {
-    if (IsMoving == true)
+        if (IsAttacking)
+        {
+            // opzionale: puoi aspettare fine animazione con coroutine
+            return;
+        }
+
+    if (IsMoving)
     {
         anim.Play("Player_Run");
+    
+        
     }
-    else
+    else if(IsMoving == false)
     {
         anim.Play("Player_Idle");
     }
 }
 
 
+private void HandleAttackInput()
+{
+    if (IsAttacking) return;
 
- 
+    comboStep++;
+    if (comboStep > 3)
+        comboStep = 1;
+
+    StartCoroutine(AttackCoroutine(comboStep));
+}
+
+
+private IEnumerator AttackCoroutine(int attackIndex)
+{
+    IsAttacking = true;
+
+    rb.velocity = Vector2.zero;
+
+    comboTimer = comboResetTime; // reset finestra combo
+
+    switch (attackIndex)
+    {
+        case 1:
+            anim.Play("Attacco1");
+            break;
+
+        case 2:
+            anim.Play("Attacco2");
+            break;
+
+        case 3:
+            anim.Play("Attacco3");
+            break;
+    }
+
+    yield return new WaitForSeconds(0.3f); // durata animazione
+
+    IsAttacking = false;
+}
+
    
 }
