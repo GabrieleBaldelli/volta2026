b273c0165875433e9c9c55a5019c1f4f4e574243
Anto <antoanza840@gmail.com>
2026-06-04 10:59:11 +0200

    Script dell'animazione iniziale commentato

--- Stage_Project_2D/Assets/Settings/Script/IntroSequence.cs
+++ Stage_Project_2D/Assets/Settings/Script/IntroSequence.cs
@@ -21,37 +21,43 @@ public class IntroSequence : MonoBehaviour
 
     private IEnumerator Start()
     {
+        // Prendo il controllo dei componenti del player e della porta
         playerMovement = player.GetComponent<PlayerMovement>();
         playerAnimator = player.GetComponent<Animator>();
         Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
         SpriteRenderer SrDoor = door.GetComponent<SpriteRenderer>();
         BoxCollider2D doorCollider = door.GetComponent<BoxCollider2D>();
 
+        // Disabilito il movimento del player
         playerRigidbody.velocity = Vector2.zero;
         playerMovement.enabled = false;
 
+        // Apro la porta
         SrDoor.sprite = openDoor;
         yield return new WaitForSeconds(openDoorDelay);
         playerAnimator.Play("Player_Run");
 
+        // Rendo il collider della porta un trigger per permettere al player di attraversarla
         doorCollider.isTrigger = true;
 
+        // Faccio camminare il player verso la posizione target
         while (Vector2.Distance(player.position, targetPosition.position) > 0.05f)
         {
             player.position = Vector2.MoveTowards(player.position, targetPosition.position, walkSpeed * Time.deltaTime);
             yield return null;
         }
 
+        // Rendo il collider della porta di nuovo solido
         doorCollider.isTrigger = false;
 
+        // Posiziono il player esattamente sulla posizione target, cambio l'animazione in idle e fermo il movimento
         player.position = targetPosition.position;
         playerAnimator.Play("Player_Idle");
         playerRigidbody.velocity = Vector2.zero;
 
+        // Chiudo la porta e riabilito il movimento del player
         SrDoor.sprite = closedDoor;
-
         yield return new WaitForSeconds(closeDoorDelay);
-
         playerMovement.enabled = true;
     }
 }
