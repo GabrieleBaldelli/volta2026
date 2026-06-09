27c5c2627e6d6a486b4f46b5a35c15f6bdf9140f
cristiancaso60-coder <cristiancaso60@gmail.com>
2026-06-09 15:23:35 +0200

    Immagini nel Main menu e menu settings

--- Stage_Project_2D/Assets/Settings/Script/Settings.cs
+++ Stage_Project_2D/Assets/Settings/Script/Settings.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class Settings : MonoBehaviour
 {
    public void MainMenu()
    {
         SceneManager.LoadScene("Main Menu");
+        EventSystem.current.SetSelectedGameObject(null);
    }
 }
