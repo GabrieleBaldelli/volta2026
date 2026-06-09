dbaa68778896433ddaec59a284ad1cdd1972b1a5
cristiancaso60-coder <cristiancaso60@gmail.com>
2026-06-09 13:15:39 +0200

    Fine script del Main menu

--- Stage_Project_2D/Assets/Settings/Script/MainMenu.cs
+++ Stage_Project_2D/Assets/Settings/Script/MainMenu.cs
@@ -1,19 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void StartGame()
     {
         SceneManager.LoadScene("Room 1");
     }
 
-    // Update is called once per frame
     public void QuitGame()
     {
+        Debug.Log("Exit button clicked");
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
