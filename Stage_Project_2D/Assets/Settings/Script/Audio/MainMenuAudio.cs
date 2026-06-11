c252d2753403daff6d51d24b8b0db20e16158988
cristiancaso60-coder <cristiancaso60@gmail.com>
2026-06-11 11:31:25 +0200

    Inizio inserimento audio nel gioco

---
+++ Stage_Project_2D/Assets/Settings/Script/Audio/MainMenuAudio.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuAudio : MonoBehaviour
+{
+    private static MainMenuAudio instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Room 1")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
