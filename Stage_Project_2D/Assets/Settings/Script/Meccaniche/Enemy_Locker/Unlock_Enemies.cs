6b0902001e676b31111a9696d3bbd7b3162edc1a
Anto <antoanza840@gmail.com>
2026-06-11 16:24:28 +0200

    aggiunti script nei trigger per bloccare e bloccare i nemici quando cambi stanza

---
+++ Stage_Project_2D/Assets/Settings/Script/Meccaniche/Enemy_Locker/Unlock_Enemies.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unlock_Enemy : MonoBehaviour
+{
+    public GameObject[] Enemies;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (GameObject enemy in Enemies)
+            {
+                if(enemy == null)
+                    continue;
+                
+                enemy.SetActive(true);
+            }
+        }
+    }
+}
