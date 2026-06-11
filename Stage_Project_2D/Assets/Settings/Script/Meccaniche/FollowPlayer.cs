488f4d0932cc79a02c73b2db9f3892289c830c91
Anto <antoanza840@gmail.com>
2026-06-11 11:58:26 +0200

    Aggiunta oscurità nella scene 4 e aggiunti i trigger per entrare nella scene 3

---
+++ Stage_Project_2D/Assets/Settings/Script/Meccaniche/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform PlayerPosition;
+    public float Speed = 1.0f;
+
+    //private Vector3 offset_Y = new Vector3(0, 2f, 0);
+
+    private void LateUpdate()
+    {
+        if (PlayerPosition == null) 
+        {
+            Debug.LogWarning("PlayerPosition is not assigned in the FollowPlayer script.");
+            return;
+        }
+
+        //offset_Y = PlayerPosition.position + offset_Y;
+
+        transform.position = PlayerPosition.position; // + offset_Y;
+        // Z = lerpSpeed * Time.deltaTime
+    }
+}
---
+++ Stage_Project_2D/Assets/Settings/Script/Meccaniche/Light_Setter/Light_Setter_False.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light_Setter_False : MonoBehaviour
+{
+    public GameObject DarkRoom;
+
+    void Start()
+    {
+        if (DarkRoom == null)
+            Debug.Log("DarkRoom non assegnata allo script \"Light_Room\".");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DarkRoom.SetActive(false);
+        }
+    }
+}
