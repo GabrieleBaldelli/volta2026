4d7b136616bfb0e53692615d15d5bc686e5cf742
Anto <antoanza840@gmail.com>
2026-06-17 15:13:53 +0200

    saving room1 after last push

--- Stage_Project_2D/Assets/Settings/Script/Shop/Loot.cs
+++ Stage_Project_2D/Assets/Settings/Script/Shop/Loot.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class Loot : MonoBehaviour
 {
+    public ItemData item;
+    public SpriteRenderer sr;
     
+    public int quantity;
+
+    private void OnValidate()
+    {
+        if (item == null)
+            return;
+
+        sr.sprite = item.icon;
+        this.name = item.name;
+    }
 }
